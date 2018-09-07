10156df267041859b1e6509c44bd482b8720f284
Charles Lee <cjl@Charless-MacBook-Pro.local>
2018-09-06 23:13:31 -0600

    created more game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,8 +1,34 @@
 console.log("Up and running!");
 
-let cardOne   = "queen"
-let cardTwo   = "queen"
-let cardThree = "king"
-let cardFour  = "king"
+// let cardOne   = "queen"
+// let cardTwo   = "queen"
+// let cardThree = "king"
+// let cardFour  = "king"
 
-console.log("User flipped " + cardOne);
+// console.log("User flipped " + cardOne);
+
+cards = ["queen", "queen", "king", "king"]
+
+cardsInPlay = []
+
+cardOne = cards[0]
+
+cardsInPlay.push(cardOne)
+console.log(cardsInPlay);
+
+cardTwo = cards[2]
+
+cardsInPlay.push(cardTwo)
+console.log(cardsInPlay);
+
+console.log("User flipped " + cardTwo);
+
+if (cardsInPlay.length === 2){
+  console.log("Yes it is");
+}
+
+if (cardsInPlay[0] === cardsInPlay[1]){
+  console.log("Nice match");
+} else {
+  console.log("Try again");
+}
